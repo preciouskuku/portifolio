b749430985be9f507ff12a339414af4c5bb4a299
Preciouskuku <preciouskukumutema@gmail.com>
2025-09-03 13:47:27 +0200

    initial commit

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,25 +6,37 @@ const projects = [
     title: 'WebScapper',
     description: 'My project using Python to scrape job data.',
     link: 'https://github.com/preciouskuku/web-scrapper.git',
-    image: '/images/webscrapper.jpg', // 🖼️ Add your screenshot here
+    image: './src/assets/web scrapper.jpg', // 🖼️ Add your screenshot here
   },
   {
     title: 'Buildlink Zimbabwe',
     description: 'A construction website for services and suppliers.',
     link: 'https://github.com/preciouskuku/buildlink-website.git',
-    image: '/images/buildlink.jpg',
+    image:'./src/assets/buildlnk.jpg',
   },
   {
     title: 'Crop Detection App',
     description: 'A smart farming app that detects crop diseases using AI and provides treatment suggestions.',
-    link: 'https://github.com/preciouskuku/crop-detection-app.git',
-    image: '/images/cropdetection.jpg',
+    link: 'https://plant-iq-rho.vercel.app/',
+    image: './src/assets/PlantIQ - Google Chrome 9_3_2025 1_21_25 PM.png',
   },
   {
     title: 'Smart Apply App',
     description: 'An intelligent platform that helps students apply for universities using AI.',
-    link: 'https://github.com/preciouskuku/smart-apply-app.git',
-    image: '/images/smartapply.jpg',
+    link: 'https://github.com/preciouskuku/CollageApp',
+    image: './src/assets/Bolt.new - Google Chrome 8_10_2025 6_47_23 PM.png',
+  },
+    {
+    title: 'Zimsister mobile App',
+    description: 'A mobile application for supporting girl during their menstrual journey.',
+    link: 'https://github.com/preciouskuku/Zimsisters',
+    image: './src/assets/Rectangle 4371.png',
+  },
+    {
+    title: 'Parking space finder',
+    description: 'A web application that help driver find available parking spaces.',
+    link: 'https://ty-6959.vercel.app/',
+    image: './src/assets/parking.jpg',
   },
 ];
 
