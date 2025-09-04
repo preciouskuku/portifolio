c91c4ecfae97193b236b0dcfd3ed33d227281910
Preciouskuku <preciouskukumutema@gmail.com>
2025-09-04 09:56:19 +0200

    initial commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,41 +8,42 @@ export default function Navbar() {
   const location = useLocation();
 
   const toggleMenu = () => setMenuOpen(!menuOpen);
+  const closeMenu = () => setMenuOpen(false);
 
   return (
     <nav className="navbar">
       <div className="navbar-logo">PRECIOUS</div>
 
-      {/* Desktop Links */}
+      {/* Desktop & Mobile Links */}
       <ul className={`navbar-links ${menuOpen ? 'active' : ''}`}>
         <li>
-          <Link to="/" className={location.pathname === '/' ? 'active' : ''}>
+          <Link to="/" className={location.pathname === '/' ? 'active' : ''} onClick={closeMenu}>
             Home
           </Link>
         </li>
         <li>
-          <Link to="/about" className={location.pathname === '/about' ? 'active' : ''}>
+          <Link to="/about" className={location.pathname === '/about' ? 'active' : ''} onClick={closeMenu}>
             About
           </Link>
         </li>
         <li>
-          <Link to="/projects" className={location.pathname === '/projects' ? 'active' : ''}>
+          <Link to="/projects" className={location.pathname === '/projects' ? 'active' : ''} onClick={closeMenu}>
             Projects
           </Link>
         </li>
         <li>
-          <Link to="/contact" className={location.pathname === '/contact' ? 'active' : ''}>
+          <Link to="/contact" className={location.pathname === '/contact' ? 'active' : ''} onClick={closeMenu}>
             Contact
           </Link>
         </li>
         <li>
-          <Link to="/hire" className={location.pathname === '/hire' ? 'active' : ''}>
+          <Link to="/hire" className={location.pathname === '/hire' ? 'active' : ''} onClick={closeMenu}>
             Hire Me
           </Link>
         </li>
       </ul>
 
-      {/* Mobile Menu Toggle */}
+      {/* Hamburger Menu */}
       <div className="menu-toggle" onClick={toggleMenu}>
         {menuOpen ? <FaTimes /> : <FaBars />}
       </div>
